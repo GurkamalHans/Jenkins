4795fd630d69070833da309311b89b3d68e63794
GurkamalHans <gurkamalhans@hotmail.com>
2018-05-31 12:21:20 -0400

    Jenkins

---
+++ plan.sh
@@ -0,0 +1,27 @@
+pkg_name=jenkins
+pkg_origin=gurkamalhans
+pkg_version=2.107.1
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="The leading open source automation server, Jenkins provides hundreds of plugins to support building, deploying and automating any project."
+pkg_license=('mit')
+pkg_upstream_url="https://jenkins.io"
+pkg_source="http://mirrors.jenkins.io/war-stable/${pkg_version}/jenkins.war"
+pkg_shasum="cec74c80190ed1f6ce55d705d2f649ddb2eaf8aba3ae26796152921d46b31280"
+pkg_deps=(core/jre8 core/curl)
+pkg_exports=(
+  [port]=jenkins.http.port
+)
+pkg_exposes=(port)
+pkg_svc_user="root"
+
+do_unpack() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  cp "${HAB_CACHE_SRC_PATH}"/"${pkg_filename}" "${pkg_prefix}"/jenkins.war
+}
