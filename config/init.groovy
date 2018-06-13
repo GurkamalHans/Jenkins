5ed52821dc01a4c0b6820d78f0379a50da168f52
GurkamalHans <gurkamalhans@hotmail.com>
2018-06-13 10:47:10 -0400

    modified init.groovy

--- config/init.groovy
+++ config/init.groovy
@@ -1,6 +1,6 @@
 import jenkins.model.Jenkins;
 
-Jenkins.instance.securityRealm.createAccount{"admin","password123"}
+Jenkins.instance.securityRealm.createAccount("admin","password123")
 
 Jenkins.instance.setSlaveAgentPort(9999)
 Jenkins.instance.save() 
