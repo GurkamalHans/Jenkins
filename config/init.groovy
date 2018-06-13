716395f50a7da141ee7aa07f6130a62872c7e55f
GurkamalHans <gurkamalhans@hotmail.com>
2018-06-13 11:26:19 -0400

    modified init.groovy

--- config/init.groovy
+++ config/init.groovy
@@ -1,6 +1,8 @@
+export JAVA_HOME="{{pkgPathFor "core/jre8"}}"
+
 import jenkins.model.Jenkins;
 
-Jenkins.instance.securityRealm.createAccount("admin","password123")
+Jenkins.instance.securityRealm.createAccount(admin,password123)
 
 Jenkins.instance.setSlaveAgentPort(9999)
 Jenkins.instance.save() 
