05d637a7eee16e8e59a07e96e355c1f9a5bfcb92
GurkamalHans <gurkamalhans@hotmail.com>
2018-06-13 10:38:43 -0400

    modified init.groovy

--- config/init.groovy
+++ config/init.groovy
@@ -1,8 +1,6 @@
 import jenkins.model.Jenkins;
 
-*Creates User account
 Jenkins.instance.securityRealm.createAccount{"admin","password123"}
 
-*Enables JNLP and sets fixed port
 Jenkins.instance.setSlaveAgentPort(9999)
 Jenkins.instance.save() 
