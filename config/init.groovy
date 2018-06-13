0143028bcaadcef40a3d8d8b2c50fa4ac26b009d
GurkamalHans <gurkamalhans@hotmail.com>
2018-06-13 13:25:37 -0400

    modified init.groovy

--- config/init.groovy
+++ config/init.groovy
@@ -1,5 +1,26 @@
 import jenkins.model.Jenkins;
 
+final List<String> REQUIRED_PLUGINS = [
+    "aws-credentials",
+    "copyartifact",
+    "git",
+    "ssh-agent",
+    "tap",
+    "workflow-aggregator",
+]
+if (Jenkins.instance.pluginManager.plugins.collect {
+        it.shortName
+    }.intersect(REQUIRED_PLUGINS).size() != REQUIRED_PLUGINS.size()) {
+    REQUIRED_PLUGINS.collect {
+        Jenkins.instance.updateCenter.getPlugin(it).deploy()
+    }.each {
+        it.get()
+    }
+    Jenkins.instance.restart()
+    println 'Run this script again after restarting to create the jobs!'
+    throw new RestartRequiredException(null)
+}
+
 Jenkins.instance.securityRealm.createAccount("admin","password123")
 
 Jenkins.instance.setSlaveAgentPort(9999)
